2c60b3415ac8d470ad26c68e08bcd2c9c4928884
Bethany Fannin <contact@bethanyfannin.com>
2026-08-27 18:24:35 +0000

    chore: remove typescript-eslint/naming-convention rule

--- eslint.config.js
+++ eslint.config.js
@@ -62,28 +62,7 @@ export default defineConfig([
         }
       ],
       '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
-      '@typescript-eslint/naming-convention': [
-        'error',
-        // enforce that private members are prefixed with an underscore
-        {
-          selector: 'memberLike',
-          modifiers: ['private'],
-          format: ['camelCase'],
-          leadingUnderscore: 'require'
-        },
-        // enforce that types generics are prefixed with 'T'
-        {
-          selector: ['typeAlias'],
-          format: ['PascalCase'],
-          prefix: ['T']
-        },
-        // enforce that interfaces are prefixed with 'I'
-        {
-          selector: ['interface'],
-          format: ['PascalCase'],
-          prefix: ['I']
-        }
-      ]
+      '@typescript-eslint/naming-convention': ['off']
     }
   },
 
