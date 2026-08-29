20625c740817fc8af7c8e4100cd633abf473f4b1
Bethany Fannin <contact@bethanyfannin.com>
2026-08-29 18:22:27 +0000

    test: moved files to match the structure of src directory

--- src/routes/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-
-const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  fastify.get('/', async () => {
-    return { message: 'Notes API reporting for duty!' };
-  });
-};
-
-export default plugin;
