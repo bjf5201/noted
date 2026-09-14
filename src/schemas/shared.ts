c351d308575db07f8b78179dedee0ede36a37ca0
Bethany Fannin <contact@bethanyfannin.com>
2026-09-14 02:46:19 +0000

    chore: include additional string schemas for email, username, etc

--- src/schemas/shared.ts
+++ src/schemas/shared.ts
@@ -21,3 +21,18 @@ export const SuccessResponse = Type.Object(
     description: 'Standard succesful operation response'
   }
 );
+
+export const StringSchema = Type.String({
+  minLength: 1,
+  maxLength: 255
+});
+
+export const EmailSchema = Type.String({
+  format: 'email',
+  minLength: 1,
+  maxLength: 255
+});
+
+export const DateTimeSchema = Type.String({ format: 'date-time' });
+
+export const IdSchema = Type.Integer({ minimum: 1 });
