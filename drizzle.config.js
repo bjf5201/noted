cb59adfdc0d76eafbf8aa0fb0549a51c266396a1
Bethany Fannin <contact@bethanyfannin.com>
2026-09-18 03:08:56 +0000

    chore: fix incorrect path in drizzle config

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   schema: './src/database/schema.ts',
   out: './src/database/migrations',
   dbCredentials: {
-    host: process.env.POSTGRES_HOST || 'localhost',
+    host: process.env.POSTGRES_HOST || 'postgres',
     port: Number(process.env.POSTGRES_PORT) || 5432,
     user: process.env.POSTGRES_USER || 'postgres',
-    password: process.env.POSTGRES_PASSWORD || '',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
     database: process.env.POSTGRES_DATABASE || 'noted_backend'
   }
 });
