3967b02f1fac7de1f12a7e22c7d75f961d1e9af7
Bethany Fannin <contact@bethanyfannin.com>
2026-09-21 03:05:01 +0000

    chore: fix lint-staged config that was missing the typecheck step

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   // run typecheck project-wide
-  '(src|test)/**/*.{ts,tsx}': [() => 'tsc --noEmit'],
+  '**/*.{ts,tsx}': [() => 'tsc --noEmit'],
 
   // run formatting and linting on staged files only
   '**/*.{ts,tsx,js,jsx}': (filenames) => [
