5870f451c0314a398b7014dc0946ff57d115dc77
Bethany Fannin <contact@bethanyfannin.com>
2026-09-21 02:41:33 +0000

    feat: add rate-limit plugin to app

---
+++ src/plugins/external/rate-limit.ts
@@ -0,0 +1,17 @@
+import fastifyRateLimit from '@fastify/rate-limit';
+import { FastifyInstance } from 'fastify';
+
+export const autoConfig = (fastify: FastifyInstance) => {
+  return {
+    max: fastify.config.RATE_LIMIT_MAX,
+    timeWindow: '1 minute'
+  };
+};
+
+/**
+ * This plugin provides a low overhead rate limiter for our routes.
+ *
+ * @see {@link https://github.com/fastify/fastify-rate-limit}
+ */
+
+export default fastifyRateLimit;
