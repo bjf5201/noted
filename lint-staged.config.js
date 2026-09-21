0d30752fd48f872f8b3a92edce2c83ad2d824ecc
Bethany Fannin <contact@bethanyfannin.com>
2026-09-21 02:59:12 +0000

    chore: add test files to typechecking for lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   // run typecheck project-wide
-  'src/**/*.{ts,tsx}': [() => 'tsc --noEmit'],
+  '(src|test)/**/*.{ts,tsx}': [() => 'tsc --noEmit'],
 
   // run formatting and linting on staged files only
   '**/*.{ts,tsx,js,jsx}': (filenames) => [
